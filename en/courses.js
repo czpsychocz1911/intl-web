6c707f82447b22e607ea88393abd67369909167b
czpsychocz1911 <czpsychocz1911@gmail.com>
2024-08-02 09:10:09 +0200

    updated localization

--- en/courses.js
+++ en/courses.js
@@ -1,5 +1,9 @@
 export default {
   'courses.courses': 'Courses',
   'courses.overview': 'Courses overview',
-  'course.create': 'Create a new course'
+  'course.create': 'Create a new course',
+  'course.title': 'Title',
+  'course.description': 'Description',
+  'course.cover':'Cover',
+  'course.visibility':'Select visibility option', 
 }
